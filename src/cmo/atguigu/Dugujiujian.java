a1fd88c941516c7985065560ed8e7edec991363b
令狐冲 <q444697374@126.com>
2017-02-28 08:43:46 +0800

    Update01

--- src/cmo/atguigu/Dugujiujian.java
+++ src/cmo/atguigu/Dugujiujian.java
@@ -4,6 +4,20 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("1111");
+		System.out.println("1111");
+
+		System.out.println("1111");
+
+		System.out.println("1111");
+
+		System.out.println("1111");
+
+		
+		System.out.println("1111");
+
+		System.out.println("1111");
+
 	}
 
 }
